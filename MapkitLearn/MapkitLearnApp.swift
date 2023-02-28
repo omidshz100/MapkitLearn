2261805f7e325794110e72e8f88067b239d8d7be
Omid Shojaeian Zanjani <46863450+omidshz100@users.noreply.github.com>
2023-02-28 11:00:37 +0100

    Initial Commit

---
+++ MapkitLearn/MapkitLearnApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapkitLearnApp.swift
+//  MapkitLearn
+//
+//  Created by Omid Shojaeian Zanjani on 28/02/23.
+//
+
+import SwiftUI
+
+@main
+struct MapkitLearnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
